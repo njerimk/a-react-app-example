e1f1529e85d7be20e53eda2ae8726834bd13476c
njerimk <nmurugikamau@gmail.com>
2020-12-10 06:35:14 -0500

    Done.

--- src/ExampleComponent.js
+++ src/ExampleComponent.js
@@ -8,6 +8,7 @@ class ExampleComponent extends Component {
         <img src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif" alt="a deeply impressed man, perhaps thinking 'whoa'"/>
         <p>Whoa!</p>
       </div>
+      
     );
   }
 }
